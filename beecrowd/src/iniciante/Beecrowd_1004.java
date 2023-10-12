b888c82c61ee623f42bdf08d577c006550122f9d
Fernando Costa <contact.fernandocosta@gmail.com>
2023-10-12 11:38:02 -0300

    First commit

---
+++ beecrowd/src/iniciante/Beecrowd_1004.java
@@ -0,0 +1,19 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Beecrowd_1004 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int PROD = a * b;
+		
+		System.out.println("PROD = " + PROD);
+
+	}
+
+}
