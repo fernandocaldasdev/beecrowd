b389650bbc10fe233f1286d1bf3b21b8445e5d6e
Fernando Costa <contact.fernandocosta@gmail.com>
2023-10-12 12:00:29 -0300

    Foi adicionado as soluções para 1001, 1002, e 1003.

---
+++ beecrowd/src/iniciante/Beecrowd_1001.java
@@ -0,0 +1,20 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Beecrowd_1001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B  = sc.nextInt();
+		int X = A + B;
+		
+		System.out.println("X = " + X);
+		
+		sc.close();
+
+	}
+
+}
