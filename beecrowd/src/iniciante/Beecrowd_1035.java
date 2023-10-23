acb6effc46dc881f658f4b6b67cf31a1f949f61b
Fernando Costa <contact.fernandocosta@gmail.com>
2023-10-23 12:09:20 -0300

    Teste de selecão

---
+++ beecrowd/src/iniciante/Beecrowd_1035.java
@@ -0,0 +1,43 @@
+package iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Beecrowd_1035 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc =  new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		
+		if(b > c) {
+			if(d > a) {
+				if((c + d) > (a + b)) {
+					if(c > 0 && d > 0) {
+						if(a % 2 == 0) {
+							System.out.println("Valores aceitos");
+						} else {
+							System.out.println("Valores não aceitos");
+						}
+					}else {
+						System.out.println("Valores não aceitos");
+					}
+				}else {
+					System.out.println("Valores não aceitos");
+				}
+			}else {
+				System.out.println("Valores não aceitos");
+			}
+		}else {
+			System.out.println("Valores não aceitos");
+		}
+		
+
+	}
+
+}
